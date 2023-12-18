c179859967bc026a8a47bfdc7ea29225dfa3b2c6
Barshon <barshonweb@gmail.com>
2023-12-18 19:16:33 +0600

    cormorant font on header links

---
+++ src/ChakraUI/Font.jsx
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Open Sans', serif`,
+    body: `'Cormorant', serif`,
+  },
+});
+
+export default theme;
--- src/Pages/Shared/Header.jsx
+++ src/Pages/Shared/Header.jsx
@@ -1,20 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo/logo.png'
+import font from '../../ChakraUI/Font'
+import { ChakraProvider } from '@chakra-ui/react';
+import "@fontsource/cormorant/400.css";
 const Header = () => {
     return (
       <div>
         <div className="bg-black text-orange-500 p-5 flex items-center px-10">
           <div>
-            <img className='md:w-1/2' src={logo} alt="" />
+            <img className="md:w-1/2" src={logo} alt="" />
           </div>
-          <div className='md:ml-52'>
-            <ul className="flex space-x-4 text-xl">
-              <Link>Home</Link>
-              <Link>Classes</Link>
-              <Link>Instructors</Link>
-            </ul>
-          </div>
+          <ChakraProvider theme={font}>
+            <div className="md:ml-52">
+              <ul className="flex space-x-4 text-xl font-bold ">
+                <Link className="hover:rounded-lg p-2 hover:bg-white">
+                  Home
+                </Link>
+                <Link className="hover:rounded-lg p-2 hover:bg-white">
+                  Classes
+                </Link>
+                <Link className="hover:rounded-lg p-2 hover:bg-white">
+                  Instructors
+                </Link>
+                <Link className="hover:rounded-lg p-2 hover:bg-white">
+                  Dashboard
+                </Link>
+                <Link className="hover:rounded-lg p-2 hover:bg-white">
+                  User
+                </Link>
+              </ul>
+            </div>
+          </ChakraProvider>
         </div>
       </div>
     );
