1a32b5a065f5c9e86bad3799b8c055a19230f186
Barshon <barshonweb@gmail.com>
2023-12-19 18:47:28 +0600

    login page design

--- src/Pages/Signin/Signin.jsx
+++ src/Pages/Signin/Signin.jsx
@@ -32,25 +32,32 @@ const Signin = () => {
 
           <Stack>
             <CardBody>
-              <div className=" rounded-xl bg-black text-orange-600 p-5">
-                <h3 className="text-3xl mb-4">Please Sign Up!</h3>
-                <form className="" onSubmit={handleSubmit(onSubmit)}>
+              <div className="flex flex-col justify-center items-center rounded-xl bg-red-900 text-yellow-500 p-5">
+                <h2 className="text-4xl mb-4 font-cursive">Summertime LevelUp</h2>
+                <h3 className="text-3xl mb-4 "> Sign Up!</h3>
+                <form className="space-y-5" onSubmit={handleSubmit(onSubmit)}>
                   <div>
+                    <label>Email</label>
+                    <br />
                     <input
+                    className="rounded p-1"
                       type="text"
                       placeholder="Email"
                       {...register("email", { required: true })}
                     />
                   </div>
                   <div>
+                    <label >Password</label>
+                    <br />
                     <input
+                    className="rounded p-1"
                       type="password"
                       placeholder="password"
                       {...register("password", { required: true })}
                     />
                   </div>
-                  <div>
-                    <input type="submit" />
+                  <div className="flex justify-center">
+                    <input className="bg-white mt-3 py-1 px-4 rounded-lg text-blue-600  hover:bg-orange-600 hover:text-white" type="submit" value="Sign Up" />
                   </div>
                 </form>
               </div>
