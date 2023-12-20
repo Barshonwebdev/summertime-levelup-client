71db55f6c4285a7b10167dfbcb22e6666f7cc8aa
Barshon <barshonweb@gmail.com>
2023-12-20 19:38:57 +0600

    banner slider (responsive)

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,109 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import slider1 from '../../../assets/banner/1.jpg'
+import slider2 from '../../../assets/banner/2.jpg'
+import slider3 from '../../../assets/banner/3.jpg'
+import slider4 from '../../../assets/banner/4.jpg'
+import slider5 from '../../../assets/banner/5.jpg'
+import slider6 from '../../../assets/banner/6.jpg'
+import slider7 from '../../../assets/banner/7.jpg'
+import slider8 from '../../../assets/banner/8.jpg'
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+// import required modules
+import { FreeMode, Pagination, } from "swiper/modules";
+
+const Banner=()=> {
+  return (
+    <div>
+      <div className="hidden md:block">
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          freeMode={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[FreeMode, Pagination]}
+          className="mySwiper md:rounded-lg"
+        >
+          <SwiperSlide className="relative">
+            <img className="z-20" src={slider1} alt="" />
+            <p className="flex justify-center items-center z-10 absolute">
+              sdsadsadasdas
+            </p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slider2} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slider3} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slider4} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slider5} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slider6} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slider7} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slider8} alt="" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <div className=" md:hidden">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          freeMode={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[FreeMode, Pagination]}
+          className="mySwiper md:rounded-lg"
+        >
+          <SwiperSlide className="relative">
+            <img className="z-20" src={slider1} alt="" />
+            <p className="flex justify-center items-center z-10 absolute">
+              sdsadsadasdas
+            </p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slider2} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slider3} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slider4} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slider5} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slider6} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slider7} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slider8} alt="" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
+export default Banner; 
+
+
--- src/Pages/Home/Homepage/Homepage.jsx
+++ src/Pages/Home/Homepage/Homepage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 
 const Homepage = () => {
     return (
-        <div>
-            <p>home</p>
+      <div>
+        <p className='text-center my-5 text-3xl px-10 md:text-5xl text-red-800 font-cursive font-bold'>Explore the hidden martial arts potential in you...</p>
+        <div className='md:mx-1'>
+          <Banner></Banner>
         </div>
+      </div>
     );
 };
 
