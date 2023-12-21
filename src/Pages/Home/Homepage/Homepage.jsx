5ccedd2e0bdc4061f16909d106a7d5874b35d8d5
Barshon <barshonweb@gmail.com>
2023-12-21 12:21:29 +0600

    homepage introductory section

--- src/Pages/Home/Homepage/Homepage.jsx
+++ src/Pages/Home/Homepage/Homepage.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
-
+import kanji from '../../../assets/kanji.jpg'
 const Homepage = () => {
     return (
       <div>
-        <div className='md:mx-1 my-10'>
+        <div className="md:mx-1 my-10">
           <Banner></Banner>
         </div>
-        <p className='text-center my-5 text-3xl px-10 md:text-5xl text-red-800 font-cursive font-bold'>Explore the hidden martial arts potential in you...</p>
+        <div className='flex justify-evenly flex-col items-center md:flex-row'>
+          <p className="text-center my-5 text-3xl px-10 md:px-36 md:text-5xl text-slate-800 font-cursive font-bold">
+            Explore the hidden martial arts potential in you
+          </p>
+          <img className='md:w-1/2' src={kanji} alt="" />
+        </div>
       </div>
     );
 };
