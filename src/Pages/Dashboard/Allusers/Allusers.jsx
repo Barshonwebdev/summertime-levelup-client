a647fbd26dd8264cfb0356a1aea37c787a49fc1b
Barshon <barshonweb@gmail.com>
2023-12-22 17:01:54 +0600

    manage all users page design (responsive)

--- src/Pages/Dashboard/Allusers/Allusers.jsx
+++ src/Pages/Dashboard/Allusers/Allusers.jsx
@@ -6,34 +6,61 @@ import {
   Th,
   Td,
   TableContainer,
+  Button,
+  Box,
 } from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
 const Allusers = () => {
+    const {data: allusers=[], refetch}=useQuery({
+        queryKey: ["allusers"],
+        queryFn:async ()=>{
+            const res= await fetch('http://localhost:5000/users');
+            return res.json();
+        }
+    })
+    console.log(allusers);
     return (
       <div>
         <h2 className="mb-10 text-3xl text-center text-orange-600 italic">
           Manage All Users
         </h2>
         <div>
-          <TableContainer>
-            <Table size={{base:"sm", md:"md", lg:"lg"}}>
-              <Thead>
-                <Tr>
-                  
-                  <Th>Email</Th>
-                  <Th>Name</Th>
-                  <Th>Role</Th>
-                  <Th>Action</Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                <Tr>
-                  <Td>inches</Td>
-                  <Td>millimetres (mm)</Td>
-                  <Td isNumeric>25.4</Td>
-                </Tr>
-              </Tbody>
-            </Table>
-          </TableContainer>
+          <Box overflowX="auto">
+            <TableContainer>
+              <Table overflowX="auto" size={{ base: "sm", md: "md", lg: "lg" }}>
+                <Thead>
+                  <Tr>
+                    <Th>Email</Th>
+
+                    <Th className="flex justify-center">Action</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {allusers.map((user) => (
+                    <Tr key={user._id}>
+                      <Td>{user.email}</Td>
+
+                      <Td className=" flex-col items-center justify-center ">
+                        <div>
+                          <Button size="sm" colorScheme="telegram">
+                            Admin
+                          </Button>{" "}
+                          <Button size="sm" colorScheme="orange">
+                            Instructor
+                          </Button>
+                        </div>
+                        <div className="flex justify-center mt-2">
+                          <Button size="sm" colorScheme="red">
+                            Delete
+                          </Button>
+                        </div>
+                      </Td>
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </Box>
         </div>
       </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,22 @@ import {  RouterProvider } from "react-router-dom";
 import router from './routes/routes';
 import { ChakraProvider } from '@chakra-ui/react';
 import AuthProvider from './providers/AuthProvider';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient(); 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <div className="">
       <ChakraProvider>
-        <AuthProvider>
-          <RouterProvider router={router} />
-        </AuthProvider>
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <RouterProvider router={router} />
+          </AuthProvider>
+        </QueryClientProvider>
       </ChakraProvider>
     </div>
   </React.StrictMode>
