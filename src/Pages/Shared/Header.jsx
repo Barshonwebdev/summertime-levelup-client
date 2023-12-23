f332f0019867aa688080f651d6fe587ce4febe0a
Barshon <barshonweb@gmail.com>
2023-12-23 13:25:46 +0600

    header design update

--- src/Pages/Shared/Header.jsx
+++ src/Pages/Shared/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 import logo from '../../assets/logo/logo.png'
+import headerimg from '../../assets/headerimg.jpg'
 import { GiHamburgerMenu } from "react-icons/gi";
 import { FaHome } from "react-icons/fa";
 import { SiGoogleclassroom } from "react-icons/si";
@@ -30,75 +31,75 @@ const Header = () => {
     return (
       <div>
         {/* desktop view  */}
-        <div className=" hidden bg-red-900  p-1 md:flex justify-between items-center px-10 md:px-0">
+        <div className=" hidden bg-yellow-50  p-1 md:flex justify-between  items-center px-10 md:px-0">
           <div>
             <div className="  rounded px-2">
-              <img
-                className="md:w-1/2  "
-                src={logo}
-                alt=""
-              />
+              <img className="md:w-1/2  " src={logo} alt="" />
             </div>
           </div>
+          <div className='flex items-center space-x-60'>
+            <div>
+              <img className="w-24" src={headerimg} alt="" />
+            </div>
+            <div className="">
+              <ul className="flex text-red-900 space-x-4 text-xl font-semibold items-center ">
+                <Link
+                  to="/"
+                  className="hover:rounded-lg p-2 hover:bg-white hover:text-red-700"
+                >
+                  Home
+                </Link>
+                <Link
+                  to="/classes"
+                  className="hover:rounded-lg p-2 hover:bg-white hover:text-red-700"
+                >
+                  Classes
+                </Link>
+                <Link
+                  to="/instructors"
+                  className="hover:rounded-lg p-2 hover:bg-white hover:text-red-700"
+                >
+                  Instructors
+                </Link>
 
-          <div className="">
-            <ul className="flex text-white space-x-4 text-xl font-semibold items-center ">
-              <Link
-                to="/"
-                className="hover:rounded-lg p-2 hover:bg-white hover:text-red-700"
-              >
-                Home
-              </Link>
-              <Link
-                to="/classes"
-                className="hover:rounded-lg p-2 hover:bg-white hover:text-red-700"
-              >
-                Classes
-              </Link>
-              <Link
-                to="/instructors"
-                className="hover:rounded-lg p-2 hover:bg-white hover:text-red-700"
-              >
-                Instructors
-              </Link>
-
-              {user ? (
-                <div className="flex space-x-4 items-center">
-                  <Link
-                    to="/dashboard"
-                    className="hover:rounded-lg p-2 hover:bg-white hover:text-red-700"
-                  >
-                    Dashboard
-                  </Link>
-                  <div className="flex space-x-2 items-center">
-                    <img className="w-7" src={user.photoURL} alt="" />
-                    <button
-                      onClick={handleLogout}
-                      className="text-sm mr-10  rounded-lg p-2 bg-white text-red-800 hover:bg-black hover:text-red-600 "
-                    >
-                      Logout
-                    </button>
-                    <p></p>
-                  </div>
-                </div>
-              ) : (
-                <div className="">
-                  <button className="text-sm mr-6 ">
+                {user ? (
+                  <div className="flex space-x-4 items-center">
                     <Link
-                      to="/signin"
-                      className="block rounded-lg p-2 bg-white text-red-800 hover:bg-black hover:text-red-600"
+                      to="/dashboard"
+                      className="hover:rounded-lg p-2 hover:bg-white hover:text-red-700"
                     >
-                      SignIn
+                      Dashboard
                     </Link>
-                  </button>
-                </div>
-              )}
-            </ul>
+                    <div className="flex space-x-2 items-center">
+                      <img className="w-7" src={user.photoURL} alt="" />
+                      <button
+                        onClick={handleLogout}
+                        className="text-sm mr-10  rounded-lg p-2 bg-white text-red-800 hover:bg-black hover:text-red-600 "
+                      >
+                        Logout
+                      </button>
+                      <p></p>
+                    </div>
+                  </div>
+                ) : (
+                  <div className="">
+                    <button className="text-sm mr-6 ">
+                      <Link
+                        to="/signin"
+                        className="block rounded-lg p-2 bg-white text-red-800 hover:bg-black hover:text-red-600"
+                      >
+                        SignIn
+                      </Link>
+                    </button>
+                  </div>
+                )}
+              </ul>
+            </div>
           </div>
         </div>
 
         {/* mobile view  */}
-        <div className="md:hidden ">
+        <div className="md:hidden bg-yellow-50 ">
           <div className="">
             <img src={logo} alt="" />
           </div>
