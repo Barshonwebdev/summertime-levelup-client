6dd1708e0a9480bf2fc64348622b3e291f87cbd8
Barshon <barshonweb@gmail.com>
2023-12-26 13:36:19 +0600

    create a class form design start

--- src/Pages/Dashboard/Createclass/Createclass.jsx
+++ src/Pages/Dashboard/Createclass/Createclass.jsx
@@ -1,10 +1,56 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Createclass = () => {
+     const {
+       register,
+       handleSubmit,
+       formState: { errors },
+     } = useForm();
+     const onSubmit = (data) => console.log(data);
+     console.log(errors);
     return (
+      <div>
+        <h3 className="text-2xl text-center my-5 text-orange-600">
+          Create a Classroom
+        </h3>
         <div>
-            create a class
+          <form className='space-y-5' onSubmit={handleSubmit(onSubmit)}>
+            <div>
+              <input
+                type="text"
+                placeholder="Class Name"
+                {...register("name", { required: true })}
+              />
+            </div>
+            <div>
+              <input
+                type="file"
+                placeholder="Class Picture"
+                {...register("picture", {})}
+              />
+            </div>
+            <div>
+              <input
+                type="number"
+                placeholder="Available Seats"
+                {...register("seats", { required: true })}
+              />
+            </div>
+            <div>
+              <input
+                type="number"
+                placeholder="Price"
+                {...register("price", { required: true })}
+              />
+            </div>
+
+            <div>
+              <input type="submit" />
+            </div>
+          </form>
         </div>
+      </div>
     );
 };
 
