e4da4b7545f4490eb38932d1062c5e2f5a24e0b9
Barshon <barshonweb@gmail.com>
2023-12-30 11:25:59 +0600

    selected classes card shown on ui

--- src/Pages/Dashboard/Selectedclasses/Selectedclasses.jsx
+++ src/Pages/Dashboard/Selectedclasses/Selectedclasses.jsx
@@ -1,16 +1,61 @@
 import React from 'react';
 import useSelected from '../../../hooks/useSelected';
-import { Button } from '@chakra-ui/react';
+import { Button, Card, CardBody, CardFooter, Heading, Image, Stack, Text } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
+import Headline from '../../../components/Headline';
 
 const Selectedclasses = () => {
     const [selectedClasses]=useSelected();
     console.log(selectedClasses);
     return (
-      <div className="text-center">
-        my selected classes
+      <div className="flex flex-col items-center">
+        <div>
+          <Headline headline={"My Selected Classes"}></Headline>
+        </div>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-5 my-10'>
+          {selectedClasses.map((eachclass) => (
+            <div key={eachclass._id}>
+              <Card className="  " height="100%" maxW="sm">
+                <CardBody>
+                  <Image
+                    className=""
+                    align={""}
+                    src={eachclass.classPhoto}
+                    borderRadius="lg"
+                  />
+                </CardBody>
+                <CardFooter>
+                  <Stack mt="6" spacing="3">
+                    <Heading className="italic text-zinc-500" size="md">
+                      {eachclass.className}
+                    </Heading>
+                    <Text color="">
+                      <div className="space-y-2">
+                        <p className="text-violet-900 font-bold text-lg">
+                          Instructor: {eachclass.instructorName}
+                        </p>
+                        <p className=" font-semibold">
+                          Available Seats:{" "}
+                          <span className="text-red-600">
+                            {eachclass.seats}
+                          </span>
+                        </p>
+                        <p className=" font-semibold">
+                          Price:{" "}
+                          <span className="text-green-600">
+                            ${eachclass.price}
+                          </span>
+                        </p>
+                      </div>
+                    </Text>
+                  </Stack>
+                </CardFooter>
+              </Card>
+            </div>
+          ))}
+        </div>
         <div className="my-5">
-          <Link to='/classes'>
+          <Link to="/classes">
             <Button colorScheme="linkedin">Back to Class Selection</Button>
           </Link>
         </div>
