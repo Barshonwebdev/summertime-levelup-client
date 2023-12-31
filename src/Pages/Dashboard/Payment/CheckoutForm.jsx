8337947c064f14802da2a9f94ffaa32da0dc2fdf
Barshon <barshonweb@gmail.com>
2023-12-31 10:56:35 +0600

    card payment by stripe

--- src/Pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,12 +1,24 @@
 import { Button } from "@chakra-ui/react";
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import useAuth from "../../../hooks/useAuth";
 
 const CheckoutForm = ({totalPrice}) => {
-    const [cardError,setCardError]=useState('');
+    
+    const {user}=useAuth();
     const stripe=useStripe();
     const elements=useElements();
     console.log(totalPrice);
+    const [cardError, setCardError] = useState("");
+    const [clientSecret, setClientSecret] = useState("");
+    useEffect(()=>{
+      axios.post(`http://localhost:5000/paymentIntent`,{totalPrice})
+      .then(res=>{
+        console.log(res.data.clientSecret);
+        setClientSecret(res.data.clientSecret);
+      })
+    },[])
     const handleSubmit=async(event)=>{
         event.preventDefault();
         if (!stripe || !elements) {
@@ -33,6 +45,30 @@ const CheckoutForm = ({totalPrice}) => {
             console.log('payment method', paymentMethod);
             setCardError('');
          }
+
+         const { paymentIntent, error:confirmError } = await stripe.confirmCardPayment(
+           clientSecret,
+           {
+             payment_method: {
+               card: card,
+               billing_details: {
+                 email:user?.email || 'Unknown',
+                 name:user?.displayName || 'Anonymous',
+               },
+             },
+           },
+         );
+        
+        if(confirmError){
+        console.log(confirmError);
+        
+        }
+
+     
+      console.log(paymentIntent)
+      
+        
+         
     }
     
     return (
@@ -60,7 +96,7 @@ const CheckoutForm = ({totalPrice}) => {
               className="mt-4"
               colorScheme="green"
               type="submit"
-              disabled={!stripe}
+              disabled={!stripe || !clientSecret}
             >
               Pay
             </Button>
